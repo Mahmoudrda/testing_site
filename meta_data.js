65d466d50cc0a8777dacfaefb3f27292984ed83e
mahmoud reda <143479086+Mahmoudrda@users.noreply.github.com>
2024-11-14 11:32:22 +0200

    Update meta_data.js

--- meta_data.js
+++ meta_data.js
@@ -6,13 +6,6 @@
     var client_id = cookieValue ? cookieValue.split('.')[2] + '.' + cookieValue.split('.')[3] : ''
     var page_type = window.location.pathname.split('/')[2].split('.')[0] || 'Home'
     if (page_type === 'index') { page_type = 'Home'; }
-    dataLayer = window.dataLayer || []
-    dataLayer.push({
-        'login_status': login_status,
-        'client_id': client_id,
-        'user_id': user_id,
-        'page_type': page_type
-    })
     document.querySelector('.login_btn').addEventListener('click', function(event) {
         if(login_status === false) {
             document.querySelector('.login_btn').innerHTML = 'Logout'
